3f20f64837d9b31d5df9d22e8a74498d2c4483fc
Francesco Zatti <francesco.zatti98gmail.com>
2023-12-12 18:19:15 +0100

    consegna parziale

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 console.log(window)
+
+fetch("https://striveschool-api.herokuapp.com/api/put-your-endpoint-here/", {
+headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NTc4NWQ0MDI2NzYxNDAwMTgzYzJlYWUiLCJpYXQiOjE3MDIzODcwMDgsImV4cCI6MTcwMzU5NjYwOH0.SPYZWut_nYM6_QUQL0K48jcLKa_yNjMjAv5fVRXKS6g"
+}
+})
