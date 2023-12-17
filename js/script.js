932c1c03ab4aa6f69952c0dfc9466bd781858aaf
Francesco Zatti <francesco.zatti98gmail.com>
2023-12-17 18:44:56 +0100

    aggiornamento

--- js/script.js
+++ js/script.js
@@ -82,16 +82,19 @@ function createProductCards() {
     .then(response => response.json())
     .then(data => data.forEach(element => {
             let card = `
-                <div class="col">
-                    <div class="card h-100 border border-warning">
-                        <img src="${element.imageUrl}" class="card-img-top" alt="${element.name}">
-                        <div class="card-body p-0">
-                            <p class="nameClass card-title fs-3 text-white fw-semibold border-bottom border-warning p-2 mb-0">${element.name}</p>
-                            <p class="descriptionClass card-text">${element.description}</p>
-                            <p class="brandClass card-text text-secondary">${element.brand}</p>
-                            <p class="priceClass card-text fs-4 text-warning">€ ${element.price}</p>
-                        </div>
-                      </div>
+                <div class="p-0" style="width: 30%;">
+                    <div class="card h-100 border border-warning pt-1 bg-dark">
+                        <a class="text-decoration-none d-flex flex-column p-0" style="width: auto;">
+                            <img src="${element.imageUrl}" class="card-img-top rounded-3 m-2" alt="${element.name}" style="height: 300px; width: auto; object-fit: cover;">
+                            <div class="card-body p-0 rounded-bottom-3 btn btn-opacity m-2" style="height: 200px; width: auto;">
+                                <p class="nameClass card-title fs-3 text-white fw-semibold border-bottom border-warning p-2 mb-0">${element.name}</p>
+                                <p class="descriptionClass card-text">${element.description}</p>
+                                <p class="brandClass card-text text-secondary">${element.brand}</p>
+                                <p class="priceClass card-text fs-4 text-warning">€ ${element.price},00</p>
+                            </div>
+                        </a>
+                        <a onClick="riempiMain()" class="goToDetails btn btn-warning my-2 mx-5">Dettagli Prodotto</a>
+                    </div>
                 </div>`;
             container.innerHTML += card;
         }))
@@ -134,4 +137,44 @@ document.getElementById("removeAll").addEventListener('click', () => {
     for (let i = 0; i < inputs.length; i++) {
       inputs[i].value = "";
     }
+  }
+
+  
+  function riempiMain() {
+    const card = document.querySelector('.card');
+    const element = {
+      imageUrl: card.querySelector('.card-img-top').getAttribute('src'),
+      name: card.querySelector('.nameClass').textContent,
+      description: card.querySelector('.descriptionClass').textContent,
+      brand: card.querySelector('.brandClass').textContent,
+      price: card.querySelector('.priceClass').textContent.replace('€', '').replace(',00', '').trim()
+    };
+  
+    const mainContent = `
+    <div class="container-fluid d-flex">
+        <img id="productImg" src="${element.imageUrl}" alt="${element.name}" style="width: 40%;">
+        <div class="mt-3 ms-3 d-flex flex-column" style="width: 45%;"> <!--PRODOTTO-->
+        <h1 class="text-warning fs-1 ms-2">${element.name}</h1>
+        <div class="text-white bg-secondary bg-opacity-25 rounded-3 p-3 d-flex justify-content-between"> 
+            <div class="d-flex flex-column me-2" style="width: 20%;">
+            <p class="bg-dark rounded-3 p-2">€ ${element.price},00</p>
+            <div class="bg-warning bg-opacity-25 rounded-3 px-2 pt-2">
+                <p class="fw-light">Spedizione: <span class="fw-semibold text-warning d-block mt-1"><img src="img/logo crudazon.png" alt="logo" style="width: 2rem;">Prime</span></p>
+            </div>
+            <div class="d-flex justify-content-between mx-2">
+                <a href="#"><button class="btn btn-warning mt-3 fw-semibold"><i class="bi bi-cart-plus"></i></button></a>
+                <a href="#"><button class="btn btn-outline-warning mt-3 fw-semibold"><i class="bi bi-heart"></i></button></a>
+            </div>
+            </div>
+            <div class="border-start border-warning ms-2" style="width: 80%;">
+            <p class="p-2 ms-2 mb-0 fw-light">${element.brand}</p>
+            <p class="bg-dark rounded-3 p-2 ms-3">${element.description}</p>
+            </div>
+        </div>
+        </div>
+    </div>
+    `;
+  
+    const main = document.querySelector('main');
+    main.innerHTML = mainContent;
   }
